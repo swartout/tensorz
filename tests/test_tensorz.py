0b6a032eb985da63404891fd85b859a6cda435d8
Carter Swartout <carterswartout@gmail.com>
2023-11-23 16:38:57 -0800

    Update TensorZ tests

--- tests/test_tensorz.py
+++ tests/test_tensorz.py
@@ -56,7 +56,7 @@ def test_add_fuzzy():
         t_c = t_a + t_b
         t_c.backward()
 
-        assert np.allclose(z_c.data, t_c.numpy())
+        assert np.allclose(z_c.data, t_c.detach().numpy())
         assert np.allclose(z_a.grad, t_b.grad.numpy())
         assert np.allclose(z_b.grad, t_b.grad.numpy())
 
@@ -90,8 +90,8 @@ def test_mul_backward():
     t_c = t_a * t_b
     t_c.backward()
 
-    assert np.allclose(z_a.grad, a.grad.numpy())
-    assert np.allclose(z_b.grad, b.grad.numpy())
+    assert np.allclose(z_a.grad, t_a.grad.numpy())
+    assert np.allclose(z_b.grad, t_b.grad.numpy())
 
 
 def test_mul_fuzzy():
@@ -109,7 +109,7 @@ def test_mul_fuzzy():
         t_c = t_a * t_b
         t_c.backward()
 
-        assert np.allclose(z_c.data, t_c.numpy())
+        assert np.allclose(z_c.data, t_c.detach().numpy())
         assert np.allclose(z_a.grad, t_a.grad.numpy())
         assert np.allclose(z_b.grad, t_b.grad.numpy())
 
@@ -150,7 +150,7 @@ def test_pow_backward():
 def test_pow_fuzzy():
     for _ in range(NUM_FUZZY):
         b = np.random.uniform(low=0.0, high=10, size=1)
-        p = np.random.randint(low=-5, high=5, size=1)
+        p = np.random.uniform(low=-5.0, high=5.0, size=1)
 
         z_b = TensorZ(b)
         z_p = TensorZ(p)
@@ -162,7 +162,7 @@ def test_pow_fuzzy():
         t_y = t_b ** t_p
         t_y.backward()
 
-        assert np.allclose(z_y.data, t_y.numpy())
+        assert np.allclose(z_y.data, t_y.detach().numpy())
         assert np.allclose(z_b.grad, t_b.grad.numpy())
         assert np.allclose(z_p.grad, t_p.grad.numpy())
 
@@ -215,8 +215,8 @@ def test_sub_fuzzy():
         t_c = t_a - t_b
         t_c.backward()
 
-        assert np.allclose(z_c.data, t_c.numpy())
-        assert np.allclose(z_a.grad, t_b.grad.numpy())
+        assert np.allclose(z_c.data, t_c.detach().numpy())
+        assert np.allclose(z_a.grad, t_a.grad.numpy())
         assert np.allclose(z_b.grad, t_b.grad.numpy())
 
 
@@ -268,6 +268,7 @@ def test_truediv_fuzzy():
         t_c = t_a / t_b
         t_c.backward()
 
+        assert np.allclose(z_c.data, t_c.detach().numpy())
         assert np.allclose(z_a.grad, t_a.grad.numpy())
         assert np.allclose(z_b.grad, t_b.grad.numpy())
 
@@ -295,7 +296,7 @@ def test_chain_ops():
     t_out = (-(t_a ** (t_b + t_d)) / t_c) - t_e
     t_out.backward()
 
-    assert np.allclose(z_out.data, t_out.numpy())
+    assert np.allclose(z_out.data, t_out.detach().numpy())
     assert np.allclose(z_a.grad, t_a.grad.numpy())
     assert np.allclose(z_b.grad, t_b.grad.numpy())
     assert np.allclose(z_c.grad, t_c.grad.numpy())
@@ -327,10 +328,51 @@ def test_chain_ops_fuzzy():
         t_out = (-(t_a ** (t_b + t_d)) / t_c) - t_e
         t_out.backward()
 
-        assert np.allclose(z_out.data, t_out.numpy())
+        assert np.allclose(z_out.data, t_out.detach().numpy())
         assert np.allclose(z_a.grad, t_a.grad.numpy())
         assert np.allclose(z_b.grad, t_b.grad.numpy())
         assert np.allclose(z_c.grad, t_c.grad.numpy())
         assert np.allclose(z_d.grad, t_d.grad.numpy())
         assert np.allclose(z_e.grad, t_e.grad.numpy())
+
+
+def test_ln_forward():
+    a = np.array([0.5, 1.0, 4.0, 9.0])
+
+    z_a = TensorZ(a)
+    z_ln = z_a.ln()
+
+    t_a = torch.tensor(a)
+    t_ln = torch.log(t_a)
+
+    assert np.allclose(z_ln.data, t_ln.numpy())
+
+
+def test_ln_backward():
+    a = np.array([4.0])
+
+    z_a = TensorZ(a)
+    z_ln = z_a.ln()
+    z_ln.backward()
+
+    t_a = torch.tensor(a, requires_grad=True)
+    t_ln = torch.log(t_a)
+    t_ln.backward()
+
+    assert np.allclose(z_a.grad, t_a.grad.numpy())
+
+
+def test_ln_fuzzy():
+    a = np.random.uniform(low=0.5, high=100, size=1)
+
+    z_a = TensorZ(a)
+    z_ln = z_a.ln()
+    z_ln.backward()
+
+    t_a = torch.tensor(a, requires_grad=True)
+    t_ln = torch.log(t_a)
+    t_ln.backward()
+
+    assert np.allclose(z_ln.data, t_ln.detach().numpy())
+    assert np.allclose(z_a.grad, t_a.grad.numpy())
 
